27aa4e22e4315298899a88f5e8259badbe167894
vimkidgit <1779156598@qq.com>
2017-10-09 17:46:21 +0800

    添加package文件

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const http = require('http');
+const http = require('http'); // 引用http模块
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => { // 创建server
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello This is KidNode web Framework\n');
+    res.end('Hello,this is KidNode web Framework\n');
 });
 
 server.listen(port, hostname, () => {
